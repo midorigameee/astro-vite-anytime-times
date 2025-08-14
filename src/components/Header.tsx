f7e4841887403f69926bc4aa3fe416ccc584c34c
midorigameee <xqbodp727@gmail.com>
2025-08-14 12:36:06 +0000

    タイトルを変更します

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Box, IconButton } from '@mui/material';
-import { Description } from '@mui/icons-material';
+import React from "react";
+import { AppBar, Toolbar, Typography, Box, IconButton } from "@mui/material";
+import { Description } from "@mui/icons-material";
 
 interface HeaderProps {
   children?: React.ReactNode;
@@ -11,7 +11,9 @@ const Header: React.FC<HeaderProps> = ({ children, onExportMarkdown }) => {
   return (
     <AppBar position="static">
       <Toolbar variant="dense">
-        <Typography variant="h6" sx={{ flexGrow: 1 }}># times-channel</Typography>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          #Anytimes
+        </Typography>
         {onExportMarkdown && (
           <IconButton color="inherit" onClick={onExportMarkdown}>
             <Description />
